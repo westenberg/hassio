97379fbd354773252393d9f1680b5b904184c1f7
L.Westenberg <l.westenberg@agrifirm.com>
2021-02-08 14:31:40 +0100

    add files

---
+++ dsmr-reader/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DB_HOST=10.0.0.11 \
+DSMRREADER_LOGLEVEL=DEBUG \
+SD_LOGLEVEL=debug \
+/run.sh
---
+++ postgres/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+POSTGRES_USER=dsmrreader \
+POSTGRES_PASSWORD=dsmrreader \
+POSTGRES_DB=dsmrreader \
+/usr/local/bin/docker-entrypoint.sh \
+postgres
